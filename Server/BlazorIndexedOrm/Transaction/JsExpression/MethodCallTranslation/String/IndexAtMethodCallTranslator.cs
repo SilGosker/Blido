ac0e1514a66383c461f055a8819dd09af4f2862e
Sil Gosker <silgosker@gmail.com>
2025-02-03 11:44:03 +0100

    add unit tests for String.[int] method

--- Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/String/IndexAtMethodCallTranslator.cs
+++ Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/String/IndexAtMethodCallTranslator.cs
@@ -6,6 +6,11 @@ public class IndexAtMethodCallTranslator : IMethodCallTranslator
 {
     public static TranslateMethodCall TranslateMethodCall => (builder, expression, processNext) =>
     {
+        if (expression.Object is not null)
+        {
+            processNext(expression.Object);
+        }
+
         builder.Append(".charAt(");
         processNext(expression.Arguments[0]);
         builder.Append(')');
@@ -14,7 +19,7 @@ public class IndexAtMethodCallTranslator : IMethodCallTranslator
     #nullable disable
     public static MethodInfo[] SupportedMethods => new[]
     {
-        typeof(string).GetMethod("get_Chars", new []{ typeof(char)}),
+        typeof(string).GetMethod("get_Chars", new []{ typeof(int)}),
     };
     #nullable restore
 }

714611784b73582781be6910d97930e9c4732dfd
Sil Gosker <silgosker@gmail.com>
2025-02-04 10:28:40 +0100

    update TrimEndMethodCallTranslator to fix unit tests

--- Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/String/TrimEndMethodCallTranslator.cs
+++ Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/String/TrimEndMethodCallTranslator.cs
@@ -6,6 +6,7 @@ public class TrimEndMethodCallTranslator : IMethodCallTranslator
 {
     public static TranslateMethodCall TranslateMethodCall => (sb, expression, processExpression) =>
     {
+        processExpression(expression.Object!);
         sb.Append(".trimEnd()");
     };
 

b5840341f1ba74194cf4473f36aff7e74760c62d
Sil Gosker <silgosker@gmail.com>
2025-02-12 14:14:12 +0100

    add Enumerable.Skip methodCallTranslation

---
+++ Server/BlazorIndexedOrm.Core.UnitTests/Transaction/JsExpression/MethodCallTranslation/Linq/SkipMethodCallTranslatorTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+
+public class SkipMethodCallTranslatorTests
+{
+    [Fact]
+    public void SupportedMethods_ShouldNotContainNull()
+    {
+        // Arrange
+        var supportedMethods = SkipMethodCallTranslator.SupportedMethods;
+        
+        // Act
+        var containsNull = supportedMethods.Contains(null);
+        
+        // Assert
+        Assert.False(containsNull);
+    }
+
+    [Fact]
+    public void TranslateMethodCall_ShouldAppendSlice()
+    {
+        // Arrange
+        var sb = new StringBuilder();
+        var method = typeof(Enumerable).GetMethods().First(e => e.Name == nameof(Enumerable.Skip)
+                                                                && e.GetParameters()[1].ParameterType == typeof(int))!
+            .MakeGenericMethod(typeof(int));
+        var expression = Expression.Call(
+            method,
+            new Expression[]
+            {
+                Expression.Constant(new List<int>()),
+                Expression.Constant(2)
+            }
+        );
+        ProcessExpression processExpression = next =>
+        {
+            if (next is ConstantExpression { Value: List<int> })
+            {
+                sb.Append("[]");
+            }
+            else if (next is ConstantExpression { Value: int i })
+            {
+                sb.Append(i);
+            }
+        };
+
+        // Act
+        SkipMethodCallTranslator.TranslateMethodCall(sb, expression, processExpression);
+        
+        // Assert
+        Assert.Equal("[].slice(2)", sb.ToString());
+    }
+}
---
+++ Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/Linq/SkipMethodCallTranslator.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+
+public class SkipMethodCallTranslator : IMethodCallTranslator
+{
+    public static TranslateMethodCall TranslateMethodCall => (builder, expression, processNext) =>
+    {
+        processNext(expression.Arguments[0]);
+        builder.Append(".slice(");
+        processNext(expression.Arguments[1]);
+        builder.Append(')');
+    };
+
+    public static MethodInfo[] SupportedMethods =>
+        typeof(Enumerable).GetMethods().Where(e => e.Name == nameof(Enumerable.Skip)).ToArray();
+}

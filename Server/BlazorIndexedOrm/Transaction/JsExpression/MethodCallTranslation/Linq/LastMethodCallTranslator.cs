54c6ae2b3e0157de3a88adaa40af7d6d43b50633
Sil Gosker <silgosker@gmail.com>
2025-02-12 14:00:57 +0100

    add Enumerable.Last methodCallTranslation

---
+++ Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/Linq/LastMethodCallTranslator.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+
+public class LastMethodCallTranslator : IMethodCallTranslator
+{
+    public static TranslateMethodCall TranslateMethodCall => (builder, expression, processNext) =>
+    {
+        builder.Append('(');
+        processNext(expression.Arguments[0]);
+        if (expression.Arguments.Count == 2)
+        {
+            builder.Append(".findLast(");
+            processNext(expression.Arguments[1]);
+            builder.Append(')');
+        }
+        else
+        {
+            builder.Append(".at(-1)");
+        }
+
+        builder.Append("??throw new Error(\"Sequence contains no matching elements\"))");
+    };
+
+    public static MethodInfo[] SupportedMethods =>
+        typeof(Enumerable).GetMethods().Where(x => x.Name == nameof(Enumerable.Last)).ToArray();
+}

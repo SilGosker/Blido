fd66487b92bdfb300d9ef309135b94d5b2f3f10a
Sil Gosker <silgosker@gmail.com>
2025-02-12 14:01:08 +0100

    add Enumerable.LastOrDefault methodCallTranslation

---
+++ Server/BlazorIndexedOrm.Core.UnitTests/Transaction/JsExpression/MethodCallTranslation/Linq/LastOrDefaultMethodCallTranslatorTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+
+public class LastOrDefaultMethodCallTranslatorTests
+{
+    [Fact]
+    public void SupportedMethods_ShouldNotContainNull()
+    {
+        // Arrange
+        var supportedMethods = LastOrDefaultMethodCallTranslator.SupportedMethods;
+
+        // Act
+        var containsNull = supportedMethods.Contains(null);
+        
+        // Assert
+        Assert.False(containsNull);
+    }
+
+    [Fact]
+    public void SupportedMethods_Length_ShouldBeEqual2()
+    {
+        // Arrange
+        var length = LastOrDefaultMethodCallTranslator.SupportedMethods.Length;
+        var expectedLength = 2;
+
+        // Assert
+        Assert.Equal(expectedLength, length);
+    }
+
+    [Fact]
+    public void TranslateMethodCall_WithoutPredicate_ShouldAppendAtMinusOneExpression()
+    {
+        // Arrange
+        var method = typeof(Enumerable).GetMethods()
+            .First(e => e.Name == nameof(Enumerable.LastOrDefault) && e.GetParameters().Length == 1)
+            .MakeGenericMethod(typeof(int));
+        var expression = Expression.Call(method, new Expression[] { Expression.Constant(new int[] { 1, 2, 3 }) });
+        var sb = new StringBuilder();
+        ProcessExpression processNext = next =>
+        {
+            if (next is ConstantExpression { Value: int[] arr })
+            {
+                sb.Append('[');
+                sb.Append(string.Join(',', arr));
+                sb.Append(']');
+            }
+        };
+
+        // Act
+        LastOrDefaultMethodCallTranslator.TranslateMethodCall(sb, expression, processNext);
+        
+        // Assert
+        Assert.Equal("[1,2,3].at(-1)", sb.ToString());
+    }
+
+    [Fact]
+    public void TranslateMethodCall_WithPredicate_ShouldAppendFindLastWithExpression()
+    {
+        // Arrange
+        var method = typeof(Enumerable).GetMethods()
+            .First(e => e.Name == nameof(Enumerable.LastOrDefault)
+                        && e.GetParameters().Length == 2
+                        && e.GetParameters()[1].ParameterType.IsGenericType
+                        && e.GetParameters()[1].ParameterType.GetGenericTypeDefinition() == typeof(Func<,>))
+            .MakeGenericMethod(typeof(int));
+        Expression<Func<int, bool>> predicate = x => x == 2;
+        var expression = Expression.Call(method, new Expression[] { Expression.Constant(new int[] { 1, 2, 3 }), predicate });
+        var sb = new StringBuilder();
+        ProcessExpression processNext = next =>
+        {
+            if (next is ConstantExpression { Value: int[] arr })
+            {
+                sb.Append('[');
+                sb.Append(string.Join(',', arr));
+                sb.Append(']');
+            } else if (next is LambdaExpression)
+            {
+                sb.Append("x => x == 2");
+            }
+        };
+
+        // Act
+        LastOrDefaultMethodCallTranslator.TranslateMethodCall(sb, expression, processNext);
+        
+        // Assert
+        Assert.Equal("[1,2,3].findLast(x => x == 2)", sb.ToString());
+    }
+}
---
+++ Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/Linq/LastOrDefaultMethodCallTranslator.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+
+public class LastOrDefaultMethodCallTranslator : IMethodCallTranslator
+{
+    public static TranslateMethodCall TranslateMethodCall => (builder, expression, processNext) =>
+    {
+        processNext(expression.Arguments[0]);
+        if (expression.Arguments.Count == 1)
+        {
+            builder.Append(".at(-1)");
+            return;
+        }
+
+        builder.Append(".findLast(");
+        processNext(expression.Arguments[1]);
+        builder.Append(')');
+    };
+
+    // LastOrDefault(IEnumerable<>)
+    // LastOrDefault(IEnumerable<>, Func<,>)
+    public static MethodInfo[] SupportedMethods => typeof(Enumerable).GetMethods().Where(e =>
+        e.Name == nameof(Enumerable.LastOrDefault)
+        && (e.GetParameters().Length == 1 ||
+            (e.GetParameters().Length == 2
+             && e.GetParameters()[1].ParameterType.IsGenericType
+             && e.GetParameters()[1].ParameterType.GetGenericTypeDefinition() == typeof(Func<,>))
+        )).ToArray();
+}

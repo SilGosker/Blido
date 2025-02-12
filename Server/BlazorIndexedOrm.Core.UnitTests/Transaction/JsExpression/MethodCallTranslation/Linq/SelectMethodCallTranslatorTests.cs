6ec7d2fd20dfacc090cedc1565472be0295e4517
Sil Gosker <silgosker@gmail.com>
2025-02-12 14:09:50 +0100

    add Enumerable.Select methodCallTranslation

---
+++ Server/BlazorIndexedOrm.Core.UnitTests/Transaction/JsExpression/MethodCallTranslation/Linq/SelectMethodCallTranslatorTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+
+public class SelectMethodCallTranslatorTests
+{
+    [Fact]
+    public void SupportedMethods_ShouldNotContainNull()
+    {
+        // Arrange
+        var supportedMethods = SelectMethodCallTranslator.SupportedMethods;
+        // Act
+        var containsNull = supportedMethods.Contains(null);
+        // Assert
+        Assert.False(containsNull);
+    }
+
+    [Fact]
+    public void TranslateMethodCall_ShouldAppendMap()
+    {
+        // Arrange
+        var sb = new StringBuilder();
+        Expression<Func<int, int>> selector = i => i * 2;
+        var method = typeof(Enumerable).GetMethods().First(e => e.Name == nameof(Enumerable.Select)
+                                                                && e.GetParameters()[1].ParameterType.GetGenericTypeDefinition() == typeof(Func<,>))
+            .MakeGenericMethod(typeof(int), typeof(int))!;
+        var expression = Expression.Call(
+            method,
+            new Expression[]
+            {
+                Expression.Constant(new List<int>()),
+                selector
+            }
+        );
+        ProcessExpression processExpression = next =>
+        {
+            if (next is ConstantExpression { Value: List<int> })
+            {
+                sb.Append("[]");
+            }
+            else if (next is LambdaExpression)
+            {
+                sb.Append("i=>i*2");
+            }
+        };
+
+        // Act
+        SelectMethodCallTranslator.TranslateMethodCall(sb, expression, processExpression);
+
+        // Assert
+        Assert.Equal("[].map(i=>i*2)", sb.ToString());
+    }
+}
---
+++ Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/Linq/SelectMethodCallTranslator.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+
+public class SelectMethodCallTranslator : IMethodCallTranslator
+{
+    public static TranslateMethodCall TranslateMethodCall => (builder, expression, processNext) =>
+    {
+        processNext(expression.Arguments[0]);
+        builder.Append(".map(");
+        processNext(expression.Arguments[1]);
+        builder.Append(')');
+    };
+
+    public static MethodInfo[] SupportedMethods => typeof(Enumerable).GetMethods()
+        .Where(m => m.Name == nameof(Enumerable.Select))
+        .ToArray();
+}

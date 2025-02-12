a36650d86db11ef0aac447a6ad8874ba590c74a8
Sil Gosker <silgosker@gmail.com>
2025-02-12 16:34:08 +0100

    add Enumerable.Sum methodCallTranslation

---
+++ Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/Linq/SumMethodCallTranslator.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+
+public class SumMethodCallTranslator : IMethodCallTranslator
+{
+    public static TranslateMethodCall TranslateMethodCall => (builder, expression, processNext) =>
+    {
+        processNext(expression.Arguments[0]);
+        if (expression.Arguments.Count == 2)
+        {
+            builder.Append(".map(");
+            processNext(expression.Arguments[1]);
+            builder.Append(')');
+        }
+        builder.Append(".reduce((a,b)=>a+b,0)");
+    };
+
+    public static MethodInfo[] SupportedMethods => typeof(Enumerable).GetMethods()
+        .Where(m => m.Name == nameof(Enumerable.Sum)).ToArray();
+}

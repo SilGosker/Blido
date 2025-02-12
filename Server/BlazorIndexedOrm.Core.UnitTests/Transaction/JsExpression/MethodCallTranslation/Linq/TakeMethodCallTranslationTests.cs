e952ee2124712c3326ec37afedd02b1cd5c1b5d3
Sil Gosker <silgosker@gmail.com>
2025-02-12 16:42:32 +0100

    add Enumerable.Take methodCallTranslation

---
+++ Server/BlazorIndexedOrm.Core.UnitTests/Transaction/JsExpression/MethodCallTranslation/Linq/TakeMethodCallTranslationTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+
+public class TakeMethodCallTranslationTests
+{
+    [Fact]
+    public void SupportedMethods_ShouldNotContainNull()
+    {
+        // Arrange
+        var supportedMethods = TakeMethodCallTranslation.SupportedMethods;
+
+        // Act
+        var containsNull = supportedMethods.Contains(null);
+        
+        // Assert
+        Assert.False(containsNull);
+    }
+
+    [Fact]
+    public void TranslateMethodCall_ShouldAppendSlice()
+    {
+        // Arrange
+        var sb = new StringBuilder();
+        var method = typeof(Enumerable).GetMethods()
+            .First(e => e.Name == nameof(Enumerable.Take)
+                        && e.GetParameters().Length == 2
+                        && e.GetParameters()[1].ParameterType == typeof(int))
+            .MakeGenericMethod(typeof(int));
+
+        var expression = Expression.Call(
+            method,
+            new Expression[]
+            {
+                Expression.Constant(new List<int>()),
+                Expression.Constant(1)
+            }
+        );
+        ProcessExpression processExpression = next =>
+        {
+            if (next is ConstantExpression { Value: List<int> })
+            {
+                sb.Append("[]");
+            }
+            else if (next is ConstantExpression { Value: int i })
+            {
+                sb.Append(i);
+            }
+        };
+
+        // Act
+        TakeMethodCallTranslation.TranslateMethodCall(sb, expression, processExpression);
+        
+        // Assert
+        Assert.Equal("[].slice(0,1)", sb.ToString());
+    }
+}

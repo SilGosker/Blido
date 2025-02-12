dbf758c7bdde38d61c652f92df6f9d6403121c05
Sil Gosker <silgosker@gmail.com>
2025-02-12 14:00:37 +0100

    add unit tests for Enumerable.FirstOrDefault method

--- Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/Linq/FirstOrDefaultMethodCallTranslator.cs
+++ Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/Linq/FirstOrDefaultMethodCallTranslator.cs
@@ -1,26 +1,32 @@
 ﻿using System.Linq.Expressions;
+using System.Reflection;
 using BlazorIndexedOrm.Core.Helpers;
 
 namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
 
-public class FirstOrDefaultMethodCallTranslator
+public class FirstOrDefaultMethodCallTranslator : IMethodCallTranslator
 {
     public static TranslateMethodCall TranslateMethodCall => (sb, expression, processNext) =>
     {
-        if (expression.Arguments.Any(e => e is not LambdaExpression lambda))
-        {
-            ThrowHelper.ThrowUnsupportedException(expression.Method);
-        }
-
-        if (expression.Arguments.Count == 1)
+        processNext(expression.Arguments[0]);
+        if (expression.Arguments.Count == 2)
         {
             sb.Append(".find(");
-            processNext(expression.Arguments[0]);
+            processNext(expression.Arguments[1]);
             sb.Append(')');
+            return;
         }
-        else
-        {
-            sb.Append("[0]");
-        }
+
+        sb.Append("[0]");
     };
+
+    // FirstOrDefault(IEnumerable<>)
+    // FirstOrDefault(IENumerable<>, Func<,>)
+    public static MethodInfo[] SupportedMethods => typeof(Enumerable).GetMethods().Where(e =>
+        e.Name == nameof(Enumerable.FirstOrDefault)
+        && (e.GetParameters().Length == 1 ||
+            (e.GetParameters().Length == 2
+             && e.GetParameters()[1].ParameterType.IsGenericType
+             && e.GetParameters()[1].ParameterType.GetGenericTypeDefinition() == typeof(Func<,>))
+        )).ToArray();
 }

7f413f5f4762de921231eec8d0f4b304748b77a9
Sil Gosker <silgosker@gmail.com>
2025-02-12 10:39:06 +0100

    add unit tests for Enumerable.ElementAtOrDefault method

--- Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/Linq/ElementAtOrDefaultMethodCallTranslator.cs
+++ Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/Linq/ElementAtOrDefaultMethodCallTranslator.cs
@@ -1,11 +1,17 @@
-﻿namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+﻿using System.Reflection;
 
-public class ElementAtOrDefaultMethodCallTranslator
+namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+
+public class ElementAtOrDefaultMethodCallTranslator : IMethodCallTranslator
 {
     public static TranslateMethodCall TranslateMethodCall => (sb, expression, processNext) =>
     {
-        sb.Append('[');
         processNext(expression.Arguments[0]);
+        sb.Append('[');
+        processNext(expression.Arguments[1]);
         sb.Append(']');
     };
+
+    public static MethodInfo[] SupportedMethods => typeof(Enumerable).GetMethods()
+        .Where(m => m.Name == nameof(Enumerable.ElementAtOrDefault)).ToArray();
 }

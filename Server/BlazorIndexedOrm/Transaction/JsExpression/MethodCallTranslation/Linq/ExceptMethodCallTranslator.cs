2bdbd64a6f98eee54cbd9d37234864f7e1084a6e
Sil Gosker <silgosker@gmail.com>
2025-02-12 10:58:29 +0100

    add unit tests for Enumerable.Except method

--- Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/Linq/ExceptMethodCallTranslator.cs
+++ Server/BlazorIndexedOrm/Transaction/JsExpression/MethodCallTranslation/Linq/ExceptMethodCallTranslator.cs
@@ -1,11 +1,19 @@
-﻿namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+﻿using System.Reflection;
 
-public class ExceptMethodCallTranslator
+namespace BlazorIndexedOrm.Core.Transaction.JsExpression.MethodCallTranslation.Linq;
+
+public class ExceptMethodCallTranslator : IMethodCallTranslator
 {
     public static TranslateMethodCall TranslateMethodCall => (sb, expression, processNext) =>
     {
-        sb.Append(".filter((_x) => ");
         processNext(expression.Arguments[0]);
-        sb.Append(".every((_y) => _x !== _y))");
+        sb.Append(".filter(_x=>!");
+        processNext(expression.Arguments[1]);
+        sb.Append(".contains(_x))");
+    };
+
+    public static MethodInfo[] SupportedMethods => new[]
+    {
+        typeof(Enumerable).GetMethods().First(e => e.Name == nameof(Enumerable.Except) && e.GetParameters().Length == 2)
     };
 }

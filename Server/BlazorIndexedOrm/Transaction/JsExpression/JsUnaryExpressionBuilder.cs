68598de0f858def1bfe872424e0695a5d9bae518
Sil Gosker <silgosker@gmail.com>
2025-02-14 21:52:45 +0100

    Removed Array.LengthMemberTranslator due to it being an Unary operator

--- Server/BlazorIndexedOrm/Transaction/JsExpression/JsUnaryExpressionBuilder.cs
+++ Server/BlazorIndexedOrm/Transaction/JsExpression/JsUnaryExpressionBuilder.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq.Expressions;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace BlazorIndexedOrm.Core.Transaction.JsExpression;
@@ -7,6 +7,19 @@ public class JsUnaryExpressionBuilder
 {
     public static void AppendUnary(StringBuilder sb, UnaryExpression expression, ProcessExpression processExpression)
     {
+
+        switch (expression.NodeType)
+        {
+            case ExpressionType.ArrayLength:
+                processExpression(expression.Operand);
+                sb.Append(".length");
+                return;
+            case ExpressionType.Convert:
+                processExpression(expression.Operand);
+                return;
+        }
+
+        sb.Append('(');
         switch (expression.NodeType)
         {
             case ExpressionType.Not:

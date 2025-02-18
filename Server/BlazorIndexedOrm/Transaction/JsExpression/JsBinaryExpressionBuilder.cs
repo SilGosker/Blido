734e98ad4bc391854e68c12344e2b8eecdf72748
Sil Gosker <silgosker@gmail.com>
2025-02-18 15:08:40 +0100

    update JsBinaryExpressionBuilder

--- Server/BlazorIndexedOrm/Transaction/JsExpression/JsBinaryExpressionBuilder.cs
+++ Server/BlazorIndexedOrm/Transaction/JsExpression/JsBinaryExpressionBuilder.cs
@@ -1,62 +1,20 @@
 ﻿using System.Linq.Expressions;
-using System.Reflection;
 using System.Text;
+using BlazorIndexedOrm.Core.Helpers;
+using BlazorIndexedOrm.Core.Transaction.JsExpression.BinaryTranslation;
 
 namespace BlazorIndexedOrm.Core.Transaction.JsExpression;
 
 public class JsBinaryExpressionBuilder
 {
-    public static void AppendEquality(StringBuilder sb, BinaryExpression binary, ProcessExpression processExpression)
+    public static void AppendBinary(StringBuilder sb, IBinaryTranslatorFactory binaryTranslatorFactory, BinaryExpression binary, ProcessExpression processExpression)
     {
-        sb.Append('(');
-        processExpression(binary.Left);
-        sb.Append(')');
-
-        switch (binary.NodeType)
+        if (binaryTranslatorFactory.TryGetValue(binary, out TranslateBinary translateBinary))
         {
-            case ExpressionType.Equal:
-                sb.Append("===");
-                break;
-            case ExpressionType.NotEqual:
-                sb.Append("!==");
-                break;
-            case ExpressionType.Add:
-                sb.Append('+');
-                break;
-            case ExpressionType.Subtract:
-                sb.Append('-');
-                break;
-            case ExpressionType.Multiply:
-                sb.Append('*');
-                break;
-            case ExpressionType.Divide:
-                sb.Append('/');
-                break;
-            case ExpressionType.Modulo:
-                sb.Append('%');
-                break;
-            case ExpressionType.GreaterThan:
-                sb.Append('>');
-                break;
-            case ExpressionType.LessThan:
-                sb.Append('<');
-                break;
-            case ExpressionType.AndAlso:
-                sb.Append("&&");
-                break;
-            case ExpressionType.OrElse:
-                sb.Append("||");
-                break;
-            case ExpressionType.GreaterThanOrEqual:
-                sb.Append(">=");
-                break;
-            case ExpressionType.LessThanOrEqual:
-                sb.Append("<=");
-                break;
+            translateBinary(sb, binary, processExpression);
+            return;
         }
 
-        sb.Append('(');
-        processExpression(binary.Right);
-        sb.Append(')');
+        ThrowHelper.ThrowUnsupportedException(binary);
     }
 }

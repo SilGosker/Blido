d487df80b0c7dd8c2996a935fa0418cd283c9e9a
Sil Gosker <silgosker@gmail.com>
2025-02-20 11:52:27 +0100

    move escaping logic to StringBuilderExtensions

--- Server/BlazorIndexedOrm.Core.UnitTests/Extensions/StringBuilderExtensionsTests.cs
+++ Server/BlazorIndexedOrm.Core.UnitTests/Extensions/StringBuilderExtensionsTests.cs
@@ -41,4 +41,275 @@ public class StringBuilderExtensionsTests
         // Assert
         Assert.Equal(expected, sb.ToString());
     }
+
+    [Fact]
+    public void AppendEscapedWithInt_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = 42;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("42", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithDouble_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = (double)42.42;
+
+        // Act
+        sb.AppendEscaped(value);
+        // Assert
+        Assert.Equal("42.42", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithFloat_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = 42.42f;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("42.42", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithUInt_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = (uint)42;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("42", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithLong_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = 42L;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("42", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithULong_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = 42ul;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("42", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithShort_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = (short)42;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("42", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithUShort_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = (ushort)42;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("42", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithByte_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = (byte)42;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("42", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithSByte_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = (sbyte)42;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("42", sb.ToString());
+    }
+
+    [Theory]
+    [InlineData('a', "'a'")]
+    [InlineData('\'', "'\\\''")]
+    public void AppendEscapedWithChar_ShouldAppendValue(char input, string expected)
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = input;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal(expected, sb.ToString());
+    }
+
+    [Theory]
+    [InlineData("test", "\"test\"")]
+    [InlineData("test\"test", "\"test\\\"test\"")]
+    public void AppendEscapedWithString_ShouldAppendValue(string input, string expected)
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = input;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal(expected, sb.ToString());
+    }
+
+    [Theory]
+    [InlineData("en-US", "\"en\"")]
+    [InlineData("de-DE", "\"de\"")]
+    public void AppendEscapedWithCultureInfo_ShouldAppendValue(string input, string expected)
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = new System.Globalization.CultureInfo(input);
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal(expected, sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithBoolTrue_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = true;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("true", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithBoolFalse_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = false;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("false", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithNull_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object? value = null;
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("null", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithIEnumerable_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = new[] { 1, 2, 3 };
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("[1,2,3]", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithIEnumerableOfIEnumerable_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = new[] { new[] { 1, 2 }, new[] { 3, 4 } };
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("[[1,2],[3,4]]", sb.ToString());
+    }
+
+    [Fact]
+    public void AppendEscapedWithIndex_ShouldAppendValue()
+    {
+        // Arrange
+        StringBuilder sb = new();
+        object value = new Index(42);
+
+        // Act
+        sb.AppendEscaped(value);
+
+        // Assert
+        Assert.Equal("42", sb.ToString());
+    }
 }
--- Server/BlazorIndexedOrm/Extensions/StringBuilderExtensions.cs
+++ Server/BlazorIndexedOrm/Extensions/StringBuilderExtensions.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Globalization;
 using System.Linq.Expressions;
 using System.Text;
@@ -42,6 +42,93 @@ internal static class StringBuilderExtensions
                     sb.Append(c);
                     break;
             }
+        }
+
+        return sb;
+    }
+
+    internal static StringBuilder AppendEscaped(this StringBuilder sb, object? obj)
+    {
+        switch (obj)
+        {
+            case Index index:
+                sb.Append(index.Value);
+                break;
+            case int i:
+                sb.Append(i);
+                break;
+            case double b:
+                sb.Append(b);
+                break;
+            case float f:
+                sb.Append(f);
+                break;
+            case uint ui:
+                sb.Append(ui);
+                break;
+            case long l:
+                sb.Append(l);
+                break;
+            case ulong ul:
+                sb.Append(ul);
+                break;
+            case short s:
+                sb.Append(s);
+                break;
+            case ushort us:
+                sb.Append(us);
+                break;
+            case byte b:
+                sb.Append(b);
+                break;
+            case sbyte @sbyte:
+                sb.Append(@sbyte);
+                break;
+            case char c:
+                sb.Append('\'');
+                if (c == '\'')
+                {
+                    sb.Append("\\\'");
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+
+                sb.Append('\'');
+                break;
+            case string s:
+                sb.Append('"');
+                sb.AppendEscaped(s);
+                sb.Append('"');
+                break;
+            case CultureInfo ci:
+                sb.Append('\"');
+                sb.Append(ci.TwoLetterISOLanguageName);
+                sb.Append('\"');
+                break;
+            case bool b:
+                sb.Append(b ? "true" : "false");
+                break;
+            case null:
+                sb.Append("null");
+                break;
+            case IEnumerable enumerable:
+                sb.Append('[');
+                var first = true;
+                foreach (var item in enumerable)
+                {
+                    if (!first)
+                    {
+                        sb.Append(',');
+                    }
+                    sb.AppendEscaped(item);
+                    first = false;
+                }
+                sb.Append(']');
+                break;
+            default:
+                throw new NotImplementedException();
         }
 
         return sb;

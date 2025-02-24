38680431729251ffbf0573002d091d30120d3280
Sil Gosker <silgosker@gmail.com>
2025-02-24 10:31:03 +0100

    add Concat MethodCallTranslator

---
+++ Server/Blido.Core.UnitTests/Transaction/JsExpression/MethodCallTranslation/Enumerable/ConcatMethodCallTranslatorTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace Blido.Core.Transaction.JsExpression.MethodCallTranslation.Enumerable;
+
+public class ConcatMethodCallTranslatorTests
+{
+    [Fact]
+    public void SupportedMethods_ShouldNotContainNull()
+    {
+        // Arrange
+        var supportedMethods = ConcatMethodCallTranslator.SupportedMethods;
+        
+        // Act
+        var containsNull = supportedMethods.Contains(null);
+        
+        // Assert
+        Assert.False(containsNull);
+    }
+
+    [Fact]
+    public void TranslateMethodCall_ShouldAppendConcat()
+    {
+        // Arrange
+        var sb = new StringBuilder();
+        var method = typeof(System.Linq.Enumerable).GetMethod(nameof(System.Linq.Enumerable.Concat))!.MakeGenericMethod(typeof(int))!;
+        var expression = Expression.Call(
+            method,
+            new Expression[]
+            {
+                Expression.Constant(new List<int>()),
+                Expression.Constant(new List<int>())
+            }
+        );
+        ProcessExpression processExpression = next =>
+        {
+            if (next is ConstantExpression { Value: List<int> })
+            {
+                sb.Append("[]");
+            }
+        };
+
+        // Act
+        ConcatMethodCallTranslator.TranslateMethodCall(sb, expression, processExpression);
+        
+        // Assert
+        Assert.Equal("[].concat([])", sb.ToString());
+    }
+}
---
+++ Server/Blido/Transaction/JsExpression/MethodCallTranslation/Enumerable/ConcatMethodCallTranslator.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace Blido.Core.Transaction.JsExpression.MethodCallTranslation.Enumerable;
+
+public class ConcatMethodCallTranslator : IMethodCallTranslator
+{
+    public static TranslateMethodCall TranslateMethodCall => (builder, expression, processNext) =>
+    {
+        processNext(expression.Arguments[0]);
+        builder.Append(".concat(");
+        processNext(expression.Arguments[1]);
+        builder.Append(')');
+    };
+
+    public static MethodInfo[] SupportedMethods => new []
+    {
+        typeof(System.Linq.Enumerable).GetMethod(nameof(System.Linq.Enumerable.Concat))!
+    };
+}

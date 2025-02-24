31dae34f88624ff12c8b5d37d12964b036a0998b
Sil Gosker <silgosker@gmail.com>
2025-02-24 10:31:29 +0100

    add Empty MethodCallTranslator

---
+++ Server/Blido.Core.UnitTests/Transaction/JsExpression/MethodCallTranslation/Enumerable/EmptyMethodCallTranslatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace Blido.Core.Transaction.JsExpression.MethodCallTranslation.Enumerable;
+
+public class EmptyMethodCallTranslatorTests
+{
+    [Fact]
+    public void SupportedMethods_ShouldNotContainNull()
+    {
+        // Arrange
+        var supportedMethods = EmptyMethodCallTranslator.SupportedMethods;
+
+        // Act
+        var containsNull = supportedMethods.Contains(null);
+
+        // Assert
+        Assert.False(containsNull);
+    }
+
+    [Fact]
+    public void TranslateMethodCall_ShouldAppendEmptyArray()
+    {
+        // Arrange
+        var sb = new StringBuilder();
+        var method = typeof(System.Linq.Enumerable).GetMethod(nameof(System.Linq.Enumerable.Empty))!.MakeGenericMethod(typeof(int));
+        var expression = Expression.Call(
+            method,
+            new Expression[] { }
+        );
+
+        // Act
+        EmptyMethodCallTranslator.TranslateMethodCall(sb, expression, _ => { });
+        
+        // Assert
+        Assert.Equal("[]", sb.ToString());
+    }
+}
---
+++ Server/Blido/Transaction/JsExpression/MethodCallTranslation/Enumerable/EmptyMethodCallTranslator.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace Blido.Core.Transaction.JsExpression.MethodCallTranslation.Enumerable;
+
+public class EmptyMethodCallTranslator : IMethodCallTranslator
+{
+    public static TranslateMethodCall TranslateMethodCall => (builder, _, _) =>
+    {
+        builder.Append("[]");
+    };
+
+    public static MethodInfo[] SupportedMethods => new MethodInfo[]
+    {
+        typeof(System.Linq.Enumerable).GetMethod(nameof(System.Linq.Enumerable.Empty))!
+    };
+}

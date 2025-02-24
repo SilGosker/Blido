4f78ebed1e096d76ccb2dcf48db9882c453e8a23
Sil Gosker <silgosker@gmail.com>
2025-02-24 10:31:17 +0100

    add Distinct MethodCallTranslator

---
+++ Server/Blido.Core.UnitTests/Transaction/JsExpression/MethodCallTranslation/Enumerable/DistinctMethodCallTranslatorTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace Blido.Core.Transaction.JsExpression.MethodCallTranslation.Enumerable;
+
+public class DistinctMethodCallTranslatorTests
+{
+    [Fact]
+    public void SupportedMethods_ShouldNotContainNull()
+    {
+        // Arrange
+        var supportedMethods = DistinctMethodCallTranslator.SupportedMethods;
+
+        // Act
+        var containsNull = supportedMethods.Contains(null);
+        
+        // Assert
+        Assert.False(containsNull);
+    }
+
+    [Fact]
+    public void TranslateMethodCall_ShouldAppendFilter()
+    {
+        // Arrange
+        var sb = new StringBuilder();
+        var method = typeof(System.Linq.Enumerable).GetMethods().First(x =>
+            x.Name == nameof(System.Linq.Enumerable.Distinct)
+            && x.GetParameters().Length == 1)!.MakeGenericMethod(typeof(int))!;
+        var expression = Expression.Call(
+            method,
+            new Expression[]
+            {
+                Expression.Constant(new List<int>())
+            }
+        );
+        ProcessExpression processExpression = next =>
+        {
+            if (next is ConstantExpression { Value: List<int> })
+            {
+                sb.Append("[]");
+            }
+        };
+
+        // Act
+        DistinctMethodCallTranslator.TranslateMethodCall(sb, expression, processExpression);
+
+        // Assert
+        Assert.Equal("[].filter((_v, _i, _a)=>_a.indexOf(_v)===_i)", sb.ToString());
+    }
+}
---
+++ Server/Blido/Transaction/JsExpression/MethodCallTranslation/Enumerable/DistinctMethodCallTranslator.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace Blido.Core.Transaction.JsExpression.MethodCallTranslation.Enumerable;
+
+public class DistinctMethodCallTranslator : IMethodCallTranslator
+{
+    public static TranslateMethodCall TranslateMethodCall => (builder, expression, processNext) =>
+    {
+        processNext(expression.Arguments[0]);
+        builder.Append(".filter((_v, _i, _a)=>_a.indexOf(_v)===_i)");
+    };
+
+    public static MethodInfo[] SupportedMethods => new[]
+    {
+        typeof(System.Linq.Enumerable)
+            .GetMethods()
+            .First(m => m.Name == nameof(System.Linq.Enumerable.Distinct) && m.GetParameters().Length == 1)
+    };
+}

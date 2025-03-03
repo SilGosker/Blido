6164b69439933214e0c0ea54ad7946286e15b6dc
Sil Gosker <silgosker@gmail.com>
2025-03-03 09:48:18 +0100

    updated MaterializationArgumentsTests

--- Server/Blido.Core.UnitTests/Transaction/Materialization/MaterializationArgumentsTests.cs
+++ Server/Blido.Core.UnitTests/Transaction/Materialization/MaterializationArgumentsTests.cs
@@ -3,12 +3,12 @@
 public class MaterializationArgumentsTests
 {
     [Theory]
-    [InlineData("  ", "  ", 0, null)]
-    [InlineData("database", "objectStore", 18, null)]
-    [InlineData("", "", ulong.MinValue, "parsedExpressions")]
-    [InlineData(null, null, ulong.MaxValue, "parsedExpressions", "parsedExpressions")]
+    [InlineData("  ", "  ", 0, "  ", null)]
+    [InlineData("database", "objectStore", 18, "", null)]
+    [InlineData("", "", ulong.MinValue, null, "parsedExpressions")]
+    [InlineData(null, null, ulong.MaxValue, "selector", "parsedExpressions", "parsedExpressions")]
 
-    public void Properties_ShouldSetValues(string database, string objectStore, ulong version, params string[]? parsedExpressions)
+    public void Properties_ShouldSetValues(string database, string objectStore, ulong version, string selector, params string[]? parsedExpressions)
     {
         // Arrange
         var arguments = new MaterializationArguments()
@@ -24,11 +24,13 @@ public class MaterializationArgumentsTests
         arguments.ObjectStore = objectStore;
         arguments.Version = version;
         arguments.ParsedExpressions = parsedExpressions;
+        arguments.Selector = selector;
 
         // Assert
         Assert.Equal(database, arguments.Database);
         Assert.Equal(objectStore, arguments.ObjectStore);
         Assert.Equal(version, arguments.Version);
         Assert.Equal(parsedExpressions, arguments.ParsedExpressions);
+        Assert.Equal(selector, arguments.Selector);
     }
 }

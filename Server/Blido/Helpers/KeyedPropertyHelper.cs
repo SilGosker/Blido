7907b31a9d764874567ab3386cce29da0c35ebaf
Sil Gosker <silgosker@gmail.com>
2025-03-18 16:30:21 +0100

    update KeyedPropertyHelper

--- Server/Blido/Helpers/KeyedPropertyHelper.cs
+++ Server/Blido/Helpers/KeyedPropertyHelper.cs
@@ -8,8 +8,72 @@ public class KeyedPropertyHelper
 {
     public static IEnumerable<PropertyInfo> GetKeys(Type type)
     {
-        return type.GetProperties().Where(x => !Attribute.IsDefined(x, typeof(NotMappedAttribute))
-                                               && (Attribute.IsDefined(x, typeof(KeyAttribute))
-                                                   || x.Name.Equals("id", StringComparison.OrdinalIgnoreCase)));
+        ArgumentNullException.ThrowIfNull(type);
+        return EnumerateKeys(type);
+    }
+
+    private static IEnumerable<PropertyInfo> EnumerateKeys(Type type)
+    {
+        bool foundKeyAttribute = false;
+        PropertyInfo? idInfo = null;
+        PropertyInfo? idCapsInfo = null;
+        PropertyInfo? idLowerInfo = null;
+        PropertyInfo? idReverseInfo = null;
+
+        foreach (PropertyInfo x in type.GetProperties())
+        {
+            if (Attribute.IsDefined(x, typeof(NotMappedAttribute)))
+            {
+                continue;
+            }
+            if (Attribute.IsDefined(x, typeof(KeyAttribute)))
+            {
+                foundKeyAttribute = true;
+                yield return x;
+            }
+            else
+            {
+                switch (x.Name)
+                {
+                    case "id":
+                        idLowerInfo = x;
+                        break;
+                    case "ID":
+                        idCapsInfo = x;
+                        break;
+                    case "Id":
+                        idInfo = x;
+                        break;
+                    case "iD":
+                        idReverseInfo = x;
+                        break;
+                }
+            }
+        }
+
+        if (foundKeyAttribute)
+        {
+            yield break;
+        }
+
+        if (idInfo != null)
+        {
+            yield return idInfo;
+        }
+
+        if (idCapsInfo != null)
+        {
+            yield return idCapsInfo;
+        }
+
+        if (idLowerInfo != null)
+        {
+            yield return idLowerInfo;
+        }
+
+        if (idReverseInfo != null)
+        {
+            yield return idReverseInfo;
+        }
     }
 }

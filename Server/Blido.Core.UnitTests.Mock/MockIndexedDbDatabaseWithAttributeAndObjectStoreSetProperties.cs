f0be74f0817e05d08206d899706be71008f45251
Sil Gosker <silgosker@gmail.com>
2025-03-27 11:09:42 +0100

    fix compilation issues

---
+++ Server/Blido.Core.UnitTests.Mock/MockIndexedDbDatabaseWithAttributeAndObjectStoreSetProperties.cs
@@ -0,0 +1,14 @@
+﻿using Blido.Core.Attributes;
+using Blido.Core.Transaction;
+
+namespace Blido.Core;
+
+[IndexedDbDatabaseName("CustomName")]
+public class MockIndexedDbDatabaseWithAttributeAndObjectStoreSetProperties : IndexedDbContext
+{
+    public MockIndexedDbDatabaseWithAttributeAndObjectStoreSetProperties(IObjectStoreFactory transactionProviderFactory) : base(transactionProviderFactory)
+    {
+    }
+
+    public ObjectStore<MockObjectStoreWithAttribute> Objects { get; set; } = null!;
+}

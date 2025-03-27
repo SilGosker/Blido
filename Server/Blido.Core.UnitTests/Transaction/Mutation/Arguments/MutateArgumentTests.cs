679122a117b55be399e14a0a511b922faa51af86
Sil Gosker <silgosker@gmail.com>
2025-03-27 09:15:39 +0100

    add MutateArguments

--- Server/Blido.Core.UnitTests/Transaction/Mutation/Arguments/MutateArgumentTests.cs
+++ Server/Blido.Core.UnitTests/Transaction/Mutation/Arguments/MutateArgumentTests.cs
@@ -25,7 +25,7 @@ public class MutateArgumentTests
     }
 
     [Fact]
-    public void CreateAsync_WithoutEntities_ThrowsArgumentOutOfRangeException()
+    public async Task CreateAsync_WithoutEntities_ThrowsArgumentOutOfRangeException()
     {
         // Arrange
         var jsRuntimeMock = new Mock<IJSRuntime>();
@@ -40,6 +40,51 @@ public class MutateArgumentTests
         async Task Act() => await MutateArguments.CreateAsync(mutationContext, cancellationToken);
         
         // Assert
-        var exception = Assert.ThrowsAsync<ArgumentOutOfRangeException>(Act);
+        await Assert.ThrowsAsync<ArgumentOutOfRangeException>(Act);
+    }
+
+    [Fact]
+    public async Task CreateAsync_WithOneOrMoreEntities_SerializesFirst()
+    {
+        // Arrange
+        var jsRuntimeMock = new Mock<IJSRuntime>();
+        var mockObjectStoreFactory = new Mock<IObjectStoreFactory>();
+        mockObjectStoreFactory.SetupGet(x => x.JsRuntime).Returns(jsRuntimeMock.Object);
+        var context = new MockIndexedDbDatabaseWithObjectStoreSetProperties(mockObjectStoreFactory.Object);
+        var mutationContext = new MutationContext(new OptionsWrapper<MutationConfiguration>(new MutationConfiguration()), new Mock<IServiceProvider>().Object, context);
+        mutationContext.Insert("first");
+        mutationContext.Insert("second");
+        var cancellationToken = CancellationToken.None;
+
+        // Act
+        var mutateArguments = await MutateArguments.CreateAsync(mutationContext, cancellationToken);
+
+        // Assert
+        Assert.Equal("""
+                     "first"
+                     """, mutateArguments.Entity);
+    }
+
+    [Fact]
+    public async Task CreateAsync_GetsVersionAndNameFromContext()
+    {
+        // Arrange
+        var jsRuntimeMock = new Mock<IJSRuntime>();
+        jsRuntimeMock.Setup(x => x.InvokeAsync<ulong>(It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<object[]>()))
+            .ReturnsAsync(1ul);
+        var mockObjectStoreFactory = new Mock<IObjectStoreFactory>();
+        mockObjectStoreFactory.SetupGet(x => x.JsRuntime).Returns(jsRuntimeMock.Object);
+        var context = new MockIndexedDbDatabaseWithObjectStoreSetProperties(mockObjectStoreFactory.Object);
+        var mutationContext = new MutationContext(new OptionsWrapper<MutationConfiguration>(new MutationConfiguration()), new Mock<IServiceProvider>().Object, context);
+        mutationContext.Insert("first");
+        var cancellationToken = CancellationToken.None;
+       
+        // Act
+        var mutateArguments = await MutateArguments.CreateAsync(mutationContext, cancellationToken);
+        
+        // Assert
+        Assert.Equal("MockIndexedDbDatabaseWithObjectStoreSetProperties", mutateArguments.Database);
+        Assert.Equal("String", mutateArguments.ObjectStore);
+        Assert.Equal(1ul, mutateArguments.Version);
     }
 }
---
+++ Server/Blido/Transaction/Mutation/Arguments/MutateArguments.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Blido.Core.Transaction.Mutation.Arguments;
+
+internal class MutateArguments
+{
+    private MutateArguments()
+    {
+    }
+    internal static async Task<MutateArguments> CreateAsync(MutationContext mutationContext, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(mutationContext);
+        var entity = mutationContext.Entities[0].BeforeChange;
+        return new MutateArguments()
+        {
+            Database = mutationContext.Database.Name,
+            ObjectStore = NameResolver.ResolveObjectStoreName(entity.GetType()),
+            Entity = JsonSerializer.Serialize(entity),
+            Version = await mutationContext.Database.GetVersionAsync(cancellationToken)
+        };
+    }
+
+    [JsonPropertyName("database")]
+    internal string Database { get; set; } = string.Empty;
+
+    [JsonPropertyName("version")]
+    internal ulong Version { get; set; }
+
+    [JsonPropertyName("objectStore")]
+    internal string ObjectStore { get; set; } = string.Empty;
+
+    [JsonPropertyName("entity")]
+    internal string Entity { get; set; } = string.Empty;
+}
